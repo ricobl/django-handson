6bfea3a0c5129a79943828f3e640c4afde8e72e7
Enrico <rico.bl@gmail.com>
2011-08-18 22:16:13 -0300

    Melhorada instalação e criada mensagem inicial.

--- install.sh
+++ install.sh
@@ -2,18 +2,34 @@
 
 NOME_VIRTUALENV="env"
 
-instala() {
-    [ -z $(which pip) ] && instala_pip
 
-	python -c 'import virtualenv' 2>/dev/null || sudo pip install virtualenv
-    cria_virtualenv
+mensagem_inicial () {
+    echo ""
+    echo "    Instalação completa."
+    echo ""
+    echo "    Execute os seguintes comandos para iniciar:"
+    echo ""
+    echo "        . $NOME_VIRTUALENV/bin/activate"
+    echo "        python manage.py runserver 0.0.0.0:8000"
+    echo ""
+    echo "    E abra a seguinte URL no browser:"
+    echo ""
+    echo "        http://0.0.0.0:8000"
+    echo ""
+}
+
+inicia () {
+    ativa
+
     cria_banco
+
+    mensagem_inicial
 }
 
 download() {
-    if [ -n $(which wget) ]; then
+    if [ $(which wget) ]; then
         wget $*
-    elif [ -n $(which curl) ]; then
+    elif [ $(which curl) ]; then
         curl -O $*
     else
         echo 'Wget ou curl não encontrados.' && exit
@@ -22,29 +38,33 @@ download() {
 
 instala_pip(){
     download http://peak.telecommunity.com/dist/ez_setup.py
-    python ez_setup.py
+    sudo python ez_setup.py
     rm ez_setup.py
 
-    easy_install pip
+    sudo easy_install pip
+}
+
+instala_virtualenv () {
+    [ -z $(which pip) ] && instala_pip
+    sudo pip install virtualenv
 }
 
 ativa(){
+    [ -d "$NOME_VIRTUALENV" ] || cria_virtualenv
 	. "$NOME_VIRTUALENV/bin/activate"
-}
-
-cria_virtualenv() {
-    [ -z $(which pip) ] && instala_pip
-
-	virtualenv "$NOME_VIRTUALENV" --no-site-packages
-
-	ativa
 
 	python -c 'import django' 2>/dev/null || pip install django==1.3
+	# python -c 'import django' 2>/dev/null || echo 'instalar django'
 	python -c 'import sqlite3' 2>/dev/null || pip install pysqlite
 }
 
+cria_virtualenv() {
+	python -c 'import virtualenv' 2> /dev/null || instala_virtualenv
+
+	virtualenv "$NOME_VIRTUALENV" --no-site-packages
+}
+
 cria_banco(){
-    ativa
     python manage.py syncdb --noinput
 }
 
@@ -52,5 +72,6 @@ limpa () {
     rm -rf build/ env/
 }
 
-[ -n "$1" ] && $1 || instala
+
+[ -n "$1" ] && $* || inicia
 
